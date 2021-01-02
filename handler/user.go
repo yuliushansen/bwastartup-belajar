1288115247ddc61e271b62326d7ea7ca97a9b863
Yulius Hansen <yuliushansen8@gmail.com>
2021-01-02 21:37:33 +0700

    Email Checker Endpoint

--- handler/user.go
+++ handler/user.go
@@ -75,3 +75,46 @@ func (h *userHandler) Login(c *gin.Context) {
 	//service mencari dengan bantuan repository user dengan email x
 	//mencocokan password
 }
+
+func (h *userHandler) CheckEmailAvailability(c *gin.Context) {
+	//input email dari user
+	//input email dimapping ke struct input
+	//struct input passing ke service
+	//service akan memanggil repository lalu mengecek email
+
+	var input user.CheckEmailInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Check Email failed", http.StatusUnprocessableEntity, "Error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	isEmailAvailable, err := h.userService.IsEmailAvailable(input)
+
+	if err != nil {
+		errorMessage := gin.H{"errors": "Server error"}
+
+		response := helper.APIResponse("Check Email failed", http.StatusUnprocessableEntity, "Error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	data := gin.H{
+		"is_available": isEmailAvailable,
+	}
+
+	metaMessage := "Email has been registered"
+
+	if isEmailAvailable {
+		metaMessage = "Email is available"
+	}
+
+	response := helper.APIResponse(metaMessage, http.StatusOK, "Success", data)
+	c.JSON(http.StatusOK, response)
+
+}
