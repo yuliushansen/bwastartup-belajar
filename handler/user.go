e71bc8fb5f17e53b16c829aa9be23548a1d74bfe
Yulius Hansen <yuliushansen8@gmail.com>
2021-01-04 10:24:49 +0700

    auth middleware

--- handler/user.go
+++ handler/user.go
@@ -152,7 +152,8 @@ func (h *userHandler) UploadAvatar(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
-	userID := 2
+	currentUser := c.MustGet("currentUser").(user.User)
+	userID := currentUser.ID
 	path := fmt.Sprintf("images/%d-%s", userID, file.Filename)
 	err = c.SaveUploadedFile(file, path)
 	if err != nil {
